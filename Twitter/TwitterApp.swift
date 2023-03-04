f5cae0ba950c108c120d04640fecf0a9680d09d8
Rohan Rajesh <dev.rohan.rajesh@gmail.com>
2023-03-04 14:55:18 +0530

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Rohan Rajesh on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
